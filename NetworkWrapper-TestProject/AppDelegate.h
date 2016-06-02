4de2322564d34287706b64778d748f8a0f1dd419
Matt Becker <matt@airsquirrels.com>
2016-06-02 13:08:30 -0400

    Added a test project for actual execution of the NetworkWrapper

---
+++ NetworkWrapper-TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkWrapper-TestProject
+//
+//  Created by Matt on 6/2/16.
+//  Copyright © 2016 Matt S Becker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#include "NetworkWrapper.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ NetworkWrapper-TestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetworkWrapper-TestProject
+//
+//  Created by Matt on 6/2/16.
+//  Copyright © 2016 Matt S Becker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
